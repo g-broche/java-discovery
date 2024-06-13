9c85cf789a57ab937645968c7cd40085bcd0dc2c
Greg <gregory.broche@yahoo.fr>
2024-06-13 22:25:09 +0200

    Tested some basic logic of user input through the command line Scanner class

--- src/Chapters/InputCourse.java
+++ src/Chapters/InputCourse.java
@@ -1,14 +1,55 @@
 package Chapters;
 
+import java.util.Scanner;
+
 import Courses.InputDemo;
 
 public class InputCourse extends BaseCourseWrapper{
     public static void runCourse(){
         InputCourse.runCourseContent("testing inputs", InputCourse::testInputs);
     }
+    /**
+     * Request user for an URL string input in terminal, in case of non valid inputed string the request is made again but
+     * it can be skipped by submitting an empty input
+     */
     private static void testInputs(){
         String validatedUrl = null;
-        validatedUrl = InputDemo.requestUrl();
-        System.out.println("entered url is : "+validatedUrl);
+        boolean isUrlValid = false;
+        boolean isUrlSkipped = false;
+        Scanner readInput = new Scanner(System.in);
+        do {
+            String inputedUrl = null;
+            inputedUrl = InputDemo.requestUrl(readInput);
+            if (InputDemo.isInputEmpty(inputedUrl)) {
+                isUrlSkipped = true;
+                break;
+            }
+            isUrlValid = InputDemo.isInputValidURLFormat(inputedUrl);
+            if (isUrlValid) {
+                validatedUrl = inputedUrl;
+            }else{
+                System.err.println("The URL selected does not have a valid format");
+            }
+        } while (!isUrlValid && !isUrlSkipped);
+        InputCourse.handleRequestResult(validatedUrl, isUrlValid, isUrlSkipped);
+        readInput.close();
+    }
+
+    /**
+     * Handle the result final result of the submitted url request, should only trigger if valid or skipped request
+     * @param url requested url that has previously been validated
+     * @param isUrlValid 
+     * @param isUrlSkipped
+     */
+    private static void handleRequestResult(String url, boolean isUrlValid ,boolean isUrlSkipped){
+        if (isUrlSkipped) {
+            System.out.println("The url request step process has been skipped");
+            return;
+        }
+        if (isUrlValid){
+            System.out.println("Entered url is : "+url);
+            return;
+        }
+        System.err.println("An unknown error occured while processing the input");
     }
 }
--- src/courses/InputDemo.java
+++ src/courses/InputDemo.java
@@ -1,14 +1,38 @@
 package Courses;
 
+import java.net.URI;
 import java.util.Scanner;
 
 public class InputDemo {
-    
-    public static String requestUrl(){
-        Scanner readInput = new Scanner(System.in);
-        System.out.println("Please enter a valid URL");
-		String url=readInput.nextLine(); 
-		readInput.close();
+    /**
+     * Request a string input compatible with a URL format
+     * @param scanner scanner instance to read the submitted input
+     * @return string submitted through command line
+     */
+    public static String requestUrl(Scanner scanner){
+        System.out.println("Please enter a valid URL or nothing to skip");
+		String url=scanner.nextLine(); 
 		return url;
+    }
+    /**
+     * Checks if an inputed string is an empty string
+     * @param inputedUrl
+     * @return true if input is an empty string, otherwise false
+     */
+    public static boolean isInputEmpty(String inputedUrl){
+        return inputedUrl == "";
+    }
+    /**
+     * Attempts to convert the inputed string into an URL to check if the format is valid
+     * @param inputedUrl string to compare to an URL format for validation
+     * @return true if format is correct, otherwise false
+     */
+    public static boolean isInputValidURLFormat(String inputedUrl){
+        try {
+            URI.create(inputedUrl).toURL();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
